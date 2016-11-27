14050a1522cbe4144e86a098011faeead51db8f1
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-11-27 22:13:54 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='idm-notification',
+    version='0.1',
+    description='Small Django app pushing model changes to kombu',
+    #long_description=open('README.rst').read(),
+    author='Alexander Dutton',
+    author_email='idm@alexdutton.co.uk',
+    url='https://github.com/alexsdutton/idm-notification',
+    license='BSD',
+    packages=find_packages(exclude=("test*", )),
+    install_requires=['kombu', 'djangorestframework'],
+)
