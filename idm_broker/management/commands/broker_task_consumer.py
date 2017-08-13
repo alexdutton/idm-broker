ba40a413cb942846d6badbfc681a9f31f8205a87
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-08-13 19:38:53 +0100

    Remove unnecessary lines

--- idm_broker/management/commands/broker_task_consumer.py
+++ idm_broker/management/commands/broker_task_consumer.py
@@ -1,16 +1,11 @@
 import logging
-from optparse import OptionParser
 
-from argparse import ArgumentParser
 from django.core.management import BaseCommand
 
 import idm_broker.consumer
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        super().add_arguments(parser)
-
     def handle(self, *args, **options):
         if options['verbosity'] > 1:
             logging.basicConfig(level=logging.DEBUG)
